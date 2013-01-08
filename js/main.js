c72e99f33bcab9d4044f7210f831f4813a018ea3
Teemu Heikkilä <sienikasvusto@pistoke.org>
2013-01-08 06:22:40 +0200

    It goes forward

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+function resizeSpacer(){
+	var h = $('footer').offset().top-($('.main-container').offset().top+$('.main-container').height())-40;
+	$('.spacer').height(h);
+}
 
+$(document).ready(function(){
+	//setInterval(resizeSpacer, 100);
+});
