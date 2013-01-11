b2bb49ea107e4d18896eca128ce432b931e313fe
Teemu Heikkilä <sienikasvusto@pistoke.org>
2013-01-12 01:35:37 +0200

    Jaa varsinaiset muutokset jäi koneelle :) eli commitin: 5be9fbde6d03252419bbacec650d5582b5dcb2b9 muutokset koodiin  Please enter the commit message for your changes. Lines starting

--- js/main.js
+++ js/main.js
@@ -4,10 +4,31 @@ function resizeSpacer(){
 }
 
 $(document).ready(function(){
+	var next = 0;
 	//setInterval(resizeSpacer, 100);
 	
+	//$('.image-carousel').carousel({
+	//	interval: 1000
+	//})
+	var items = $('.carousel-description > .d-item');
 	$('.carousel').carousel({
 		interval: 8000
-	})
+	}).on('slide', function(){
+		//console.log(items.length);
+		var itemcount = items.length;
+		$('.carousel-description > .active').hide('drop',{ direction: "down" }, 250, function(){
+			$(this).removeClass('active');
+		});
+		if(next < itemcount-1){
+			next++;
+		}else{
+			next=0;
+		}
+		
+		$(items[next]).delay(250).prependTo('.carousel-description').show('drop', { direction: "up" }, 250, function(){
+			$(this).addClass('active');
+		});
+		//$('.carousel-description:first-child');
+	});
 	
 });
