7bb5703136484ad54389b33d871790c97fd14035
Teemu Heikkilä <sienikasvusto@pistoke.org>
2017-01-06 18:48:18 +0200

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -3,17 +3,17 @@
 		<div class="row">
 			<div class="span4 downbox">
 				Lanttu ry</br>
-				Päiväkummuntie 4</br>
-				05400 Jokela</br>
+				Kaukolantie 67</br>
+				04500 Kellokoski</br>
 				<?php //echo date('Y'); ?> <?php //bloginfo('name'); ?>	
 			</div>
 			<div class="span4 downbox">
 				&nbsp;
 			</div>
 			<div class="span4 some-right">
-				<a href="http://www.facebook.com/groups/lanttury/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/picons46.png" alt="Facebook" /></a>
+				<a href="https://www.facebook.com/JokeLANparty/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/picons46.png" alt="Facebook" /></a>
 				<a href="https://twitter.com/lanttury/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/picons43.png" alt="Twitter" /></a>
-				<a href="http://www.youtube.com/user/TourLANchannel/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/picons58.png" alt="Youtube" /></a>
+				<a href="https://www.youtube.com/user/JokeLANchannel" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/picons58.png" alt="Youtube" /></a>
 			</div>
 		</div>
 	</div>
